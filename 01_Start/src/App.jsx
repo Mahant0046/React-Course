5eb54a950d517df1b20e9ad277d189e7cb470f05
Mahantesh <gudisagarmahantesh720@gmail.com>
2026-01-06 10:38:08 +0530

    First Commit

---
+++ 01_Start/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Welcome to the world of React</h1>
+      <h5>A Powerfull Javascript Library</h5>
+    </>
+  )
+}
+
+export default App
